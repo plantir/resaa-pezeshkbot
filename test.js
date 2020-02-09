1c938912e4c356a530338890e7304d7405306645
armin <arminkheirkhahan@gmail.com>
2020-02-09 20:58:44 +0330

    fix ture

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const Bot = require('node-telegram-bot-api');
 let bot = new Bot('646189637:AAFLDZMefpHpm8MQobqv468Vw0iBotLlYC8', {
-  polling: ture
+  polling: true
 });
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, 'hi');
