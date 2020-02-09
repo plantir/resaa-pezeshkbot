7d15e1581ce94913aa34ec4c7ccf980be760e2a2
armin <arminkheirkhahan@gmail.com>
2020-02-09 21:08:23 +0330

    test

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
-const Bot = require('node-telegram-bot-api');
-let bot = new Bot('646189637:AAFLDZMefpHpm8MQobqv468Vw0iBotLlYC8', {
-  polling: true
-});
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'hi');
-});
+// const Bot = require('node-telegram-bot-api');
+// let bot = new Bot('646189637:AAFLDZMefpHpm8MQobqv468Vw0iBotLlYC8', {
+//   polling: true
+// });
+// bot.on('message', msg => {
+//   bot.sendMessage(msg.chat.id, 'hi');
+// });
+
+const axios = require('axios');
+
+axios
+  .get(
+    'https://api.telegram.org/bot646189637:AAFLDZMefpHpm8MQobqv468Vw0iBotLlYC8/getUpdates'
+  )
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
