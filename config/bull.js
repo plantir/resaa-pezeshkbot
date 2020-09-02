522744514761377898c11a912085e7af30c99da1
armin <arminkheirkhahan@gmail.com>
2020-09-02 11:58:29 +0430

    change redis default conection for redis

--- config/bull.js
+++ config/bull.js
@@ -7,7 +7,7 @@ module.exports = {
   connection: Env.get('BULL_CONNECTION', 'bull'),
   bull: {
     redis: {
-      host: Env.get('REDIS_CONNECTION', 'localhost'),
+      host: Env.get('REDIS_HOST', 'localhost'),
       port: 6379,
       password: null,
       db: 0,
