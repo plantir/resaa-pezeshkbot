66c768e1f6869454b86fcbff901e878cc7d915e5
armin <arminkheirkhahan@gmail.com>
2020-10-25 14:56:14 +0330

    add guid to orders

--- app/Models/Hooks/CoronaOrderHook.js
+++ app/Models/Hooks/CoronaOrderHook.js
@@ -1,7 +1,13 @@
 'use strict';
-
+const Token = use('Token')
 const CoronaOrderHook = (exports = module.exports = {});
 
+CoronaOrderHook.beforeCreate = async (modelInstance) => {
+  modelInstance.guid = Token.generate(8)
+  return modelInstance
+  // modelInstance.transaction_id = transaction.id;
+  // transaction.save();
+};
 CoronaOrderHook.afterCreate = async (modelInstance) => {
   await modelInstance.transaction().create({
     amount: modelInstance.prepay_amount,
