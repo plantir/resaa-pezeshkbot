2b0845750b65db85a84adab87be9a085568e9797
armin <arminkheirkhahan@gmail.com>
2020-12-28 11:45:13 +0330

    fix orderby ASC

--- app/Controllers/Http/Site/CoronaCityController.js
+++ app/Controllers/Http/Site/CoronaCityController.js
@@ -6,9 +6,9 @@ class CityController {
     return City.query()
       .has('tests', (builder) => builder.where({ is_deleted: false }))
       .with('tests', (builder) =>
-        builder.where({ is_deleted: false }).orderBy('sort_order', 'Desc')
+        builder.where({ is_deleted: false }).orderBy('sort_order', 'ASC')
       )
-      .orderBy('sort_order', 'Desc')
+      .orderBy('sort_order', 'ASC')
       .where({ is_deleted: false })
       .fetch();
   }
@@ -17,7 +17,7 @@ class CityController {
     return CoronaTest.query()
       .where({ city_id })
       .where({ is_deleted: false })
-      .orderBy('sort_order', 'DESC')
+      .orderBy('sort_order', 'ASC')
       .fetch();
   }
 }
